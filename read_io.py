3005a0b7626aa8c83448b35370c22d090a552d4b
ahmedhan38 <ahmed-han_@hotmail.de>
2022-11-20 21:09:36 +0100

    Added the configered files

--- read_io.py
+++ read_io.py
@@ -6,22 +6,22 @@ import numpy as np
 from scipy.io import wavfile
 import scipy.io
 from scipy.io.wavfile import write
-translate_dict = { 'A':'.-', 'B':'-...',
-                    'C':'-.-.', 'D':'-..', 'E':'.',
-                    'F':'..-.', 'G':'--.', 'H':'....',
-                    'I':'..', 'J':'.---', 'K':'-.-',
-                    'L':'.-..', 'M':'--', 'N':'-.',
-                    'O':'---', 'P':'.--.', 'Q':'--.-',
-                    'R':'.-.', 'S':'...', 'T':'-',
-                    'U':'..-', 'V':'...-', 'W':'.--',
-                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
-                    '1':'.----', '2':'..---', '3':'...--',
-                    '4':'....-', '5':'.....', '6':'-....',
-                    '7':'--...', '8':'---..', '9':'----.',
-                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
-                    '?':'..--..', '/':'-..-.', '-':'-....-',
-                    '(':'-.--.', ')':'-.--.-'}
 
+translate_dict = {'A': '.-', 'B': '-...',
+                  'C': '-.-.', 'D': '-..', 'E': '.',
+                  'F': '..-.', 'G': '--.', 'H': '....',
+                  'I': '..', 'J': '.---', 'K': '-.-',
+                  'L': '.-..', 'M': '--', 'N': '-.',
+                  'O': '---', 'P': '.--.', 'Q': '--.-',
+                  'R': '.-.', 'S': '...', 'T': '-',
+                  'U': '..-', 'V': '...-', 'W': '.--',
+                  'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                  '1': '.----', '2': '..---', '3': '...--',
+                  '4': '....-', '5': '.....', '6': '-....',
+                  '7': '--...', '8': '---..', '9': '----.',
+                  '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                  '?': '..--..', '/': '-..-.', '-': '-....-',
+                  '(': '-.--.', ')': '-.--.-'}
 
 sample_rate, data = wavfile.read("decode_me.wav")
 print("The sample rate is: " + str(sample_rate) + " Hz")
@@ -30,9 +30,8 @@ length = data.shape[0] / sample_rate
 print(f"length = {length}s")
 
 time = np.linspace(0., length, data.shape[0])
-#plt.plot(time, data)           //Comment them out for the audio graph
-#plt.plot(time, data)           //Comment them out for the audio graph
-plt.xlabel("Time [s]")
-plt.ylabel("Amplitude")
-#plt.show()                     //Comment them out for the audio graph
-
+# plt.plot(time, data)           //Comment me out for the audio graph
+# plt.plot(time, data)           //Comment me out for the audio graph
+# plt.xlabel("Time [s]")        //Comment me out
+# plt.ylabel("Amplitude")       //Comment me out
+# plt.show()                     //Comment me out for the audio graph
--- write_io.py
+++ write_io.py
@@ -3,24 +3,25 @@ from playsound import playsound
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.io import wavfile
+
 import scipy.io
 from scipy.io.wavfile import write
 
-translate_dict = { 'A':'.-', 'B':'-...',
-                    'C':'-.-.', 'D':'-..', 'E':'.',
-                    'F':'..-.', 'G':'--.', 'H':'....',
-                    'I':'..', 'J':'.---', 'K':'-.-',
-                    'L':'.-..', 'M':'--', 'N':'-.',
-                    'O':'---', 'P':'.--.', 'Q':'--.-',
-                    'R':'.-.', 'S':'...', 'T':'-',
-                    'U':'..-', 'V':'...-', 'W':'.--',
-                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
-                    '1':'.----', '2':'..---', '3':'...--',
-                    '4':'....-', '5':'.....', '6':'-....',
-                    '7':'--...', '8':'---..', '9':'----.',
-                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
-                    '?':'..--..', '/':'-..-.', '-':'-....-',
-                    '(':'-.--.', ')':'-.--.-'}
+translate_dict = {'A': '.-', 'B': '-...',
+                  'C': '-.-.', 'D': '-..', 'E': '.',
+                  'F': '..-.', 'G': '--.', 'H': '....',
+                  'I': '..', 'J': '.---', 'K': '-.-',
+                  'L': '.-..', 'M': '--', 'N': '-.',
+                  'O': '---', 'P': '.--.', 'Q': '--.-',
+                  'R': '.-.', 'S': '...', 'T': '-',
+                  'U': '..-', 'V': '...-', 'W': '.--',
+                  'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                  '1': '.----', '2': '..---', '3': '...--',
+                  '4': '....-', '5': '.....', '6': '-....',
+                  '7': '--...', '8': '---..', '9': '----.',
+                  '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                  '?': '..--..', '/': '-..-.', '-': '-....-',
+                  '(': '-.--.', ')': '-.--.-'}
 
 message = "Ahmedhan"
 message = " ".join(translate_dict[c] for c in message.upper())
@@ -38,9 +39,17 @@ def play_morse_code(message):
         else:
             print("Invalid")
 
-print(message)
-#play_morse_code(message)
 
-reverse_dict = {v: k for k, v in translate_dict.items()}
-reverse_message = "".join(reverse_dict[c] for c in message.split(" "))
-print(reverse_message)
+print(message)
+play_morse_code(message)  # plays the sound of the encoded string
+
+# Write kann keine strings auslesen dafuer habe ich den Ton direkt ausgegeben ohen die Datei
+# wavfile.write("myName1.wav", 7119, message)
+
+
+## Nachricht rueckwaerts
+# reverse_dict = {v: k for k, v in translate_dict.items()}
+# reverse_message = "".join(reverse_dict[c] for c in message.split(" "))
+# print(reverse_message)
+
+
